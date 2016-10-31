cd828e345278bddd965fdbf17bcc56c312c0a013
Nicolapps <webmaster@nicolapps.ch>
2016-10-31 06:30:36 +0100

    Added a form to add websites

--- JustDoIt.safariextension/js/settingsPopover.js
+++ JustDoIt.safariextension/js/settingsPopover.js
@@ -1,4 +1,20 @@
 
+Vue.component('websites-list', {
+    props: ['websites'],
+
+    template: '#websites-list-template',
+
+    methods: {
+        remove: function(index) {
+            this.websites.splice(index, 1);
+        }
+    }
+});
+
 new Vue({
-    el: 'body'
+    el: '#settings',
+
+    data: {
+        websites: ['facebook.com', 'twitter.com']
+    }
 });
