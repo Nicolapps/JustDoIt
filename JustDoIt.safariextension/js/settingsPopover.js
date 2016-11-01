8a6773a9c4fcdfcacf92d65cdeafe71ac899699c
Nicolapps <webmaster@nicolapps.ch>
2016-11-01 22:00:33 +0100

    Add and remove websites

--- JustDoIt.safariextension/js/settingsPopover.js
+++ JustDoIt.safariextension/js/settingsPopover.js
@@ -1,12 +1,32 @@
 
+// Fetch the blocked websites list from Safari settings
+var blockedWebsites = eval(safari.extension.settings.blockedWebsites);
+
 Vue.component('websites-list', {
     props: ['websites'],
 
     template: '#websites-list-template',
 
+    data: function(){
+        return {
+            'addedWebsite': ''
+        }
+    },
+
     methods: {
+        addWebsite: function() {
+            this.websites.push(this.addedWebsite);
+            this.addedWebsite = '';
+            this.updateSafariSettings();
+        },
+
         remove: function(index) {
             this.websites.splice(index, 1);
+            this.updateSafariSettings();
+        },
+
+        updateSafariSettings: function() {
+            safari.extension.settings.blockedWebsites = JSON.stringify(this.websites);
         }
     }
 });
@@ -15,6 +35,6 @@ new Vue({
     el: '#settings',
 
     data: {
-        websites: ['facebook.com', 'twitter.com']
+        websites: blockedWebsites
     }
 });
